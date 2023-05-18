14e554555358b2010ffc6320aed1c0b4a86e0877
Varun <varun769097@gmail.com>
2023-05-19 04:35:41 +0530

    forntend url added

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://wedesigne.vercel.app/',
   credentials: true
 }));
 
