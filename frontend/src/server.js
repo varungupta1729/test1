251ac23457148baf50b86b6423405cb818a80343
Varun <varun769097@gmail.com>
2023-05-19 04:27:16 +0530

    Backend url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://wedesigne.onrender.com/api/v2";
+export const server = "https://test1-6ln2-erq68suiy-varungupta1729.vercel.app/api/v2";
 
-export const backend_url = "https://wedesigne.onrender.com";
+export const backend_url = "https://test1-6ln2-erq68suiy-varungupta1729.vercel.app.com/";
